3290d17d4aac3dddcaa492ab788fb1a5758aba4a
Evee ⭐ <115804333+EvelynGonzalez21@users.noreply.github.com>
2023-10-23 18:26:24 +0000

    🎃FEATURE: App Agregar visualiazacion condicionada del formulario

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,37 @@
 import { ref } from 'vue';
 const header = ref('App Lista de compras');
 const items = ref([
-  {id: 1, label: '10 bolillos'},
-  {id: 2, label: '1 lata de frijoles'},
-  {id: 3, label: '2 lata de atún'}
+ // {id: 1, label: '10 bolillos'},
+ // {id: 2, label: '1 lata de frijoles'},
+  // {id: 3, label: '2 lata de atún'}
 ]);
 // Agregando metodo para guardar nuevo articulo en la lista
 const saveItem =() => {
   items.value.push({id: items.value.length + 1, label: newItem.value})
-  // Limpiando el contenido de 
-  newItem.value ="";
+  // Limpiando el contenido de la caja de txt
+  newItem.value = "";
 };
 const newItem = ref('');
 const newItemHighPriority = ref(false);
+const editing = ref(false);
+const doEdit = (edit) => {
+  //altero la variable "editing"
+  editing.value = edit;
+  // Limpiando el inpunt del txt
+  newItem.value = "";
+};
 </script>
 <template>
-  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-  <form v-on:submit.prevent="saveItem" class="add-item form">
-    <!-- Input de Nuevo Articulo -->
-    <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
-    <!-- Check Boxes -->
-    <label>
+   <div class="header">
+    <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+   <button v-if="!editing" @click="doEdit(true)" class="btn btn-primary"> Agregar articulo</button>
+   <button v-else @click="doEdit(false)" class="btn">Cancelar</button>  
+</div>
+<form v-if="editing" v-on:submit.prevent="saveItem" class="add-item form">
+  <!-- Input de Nuevo Articulo -->
+  <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
+  <!-- Check Boxes -->
+  <label>
       <input v-model="newItemHighPriority" 
       type="checkbox">
       Alta Prioridad
@@ -35,6 +46,8 @@ const newItemHighPriority = ref(false);
       🧸 {{ label }}
     </li>
   </ul>
+  <p v-if="items.length === 0">🥀Lista de compras vacia🥀</p>
+  
 </template>
 <style scoped>
 .shopping-cart-icon {
